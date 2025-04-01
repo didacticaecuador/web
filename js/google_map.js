5206dae62e9a42601a15bfc4b52501e03d9f8e0b
didacticaecuador <96453658+didacticaecuador@users.noreply.github.com>
2025-03-31 21:04:47 -0500

    Update google_map.js

--- js/google_map.js
+++ js/google_map.js
@@ -5,13 +5,13 @@ function init() {
     // Basic options for a simple Google Map
     // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
     // var myLatlng = new google.maps.LatLng(40.71751, -73.990922);
-    var myLatlng = new google.maps.LatLng(40.69847032728747, -73.9514422416687);
+    var myLatlng = new google.maps.LatLng(19.4236295, -99.16330519999997);
     // 39.399872
     // -8.224454
     
     var mapOptions = {
         // How zoomed in you want the map to start at (always required)
-        zoom: 7,
+        zoom: 11,
 
         // The latitude and longitude to center the map (always required)
         center: myLatlng,
